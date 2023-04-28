5b9c8d83d260370abb292a462722dd98f8dabedb
Bibek Ranjan Saha <PK98cXYv3zg7a7t>
2023-04-28 16:26:52 +0530

    Added build files

---
+++ custom.js
@@ -0,0 +1,49 @@
+function showAlert(msg) {
+    alert(msg)
+}
+
+function getBatteryDataInternal() {
+ navigator.getBattery()
+    .then(battery => {
+      let navigatorData = {
+        charging: battery.charging,
+        chargingTime: battery.chargingTime,
+        dischargingTime: battery.dischargingTime,
+        level: battery.level,
+        status: true
+      };
+        const testDiv = document.createElement("div");
+        testDiv.setAttribute("id", "test");
+        testDiv.style.display = 'none';
+        testDiv.textContent = `${JSON.stringify(navigatorData)}`;
+        document.body.appendChild(testDiv);
+    })
+    .catch(() => {
+      let navigatorData = {
+        status: false
+      };
+    const testDiv = document.createElement("div");
+    testDiv.setAttribute("id", "test");
+    testDiv.style.display = 'none';
+    testDiv.textContent = `${JSON.stringify(navigatorData)}`;
+    document.body.appendChild(testDiv);
+    });
+}
+
+function returnBackBatteryData(){
+    const myDiv = document.getElementById("test");
+    const divText = myDiv.textContent;
+    myDiv.remove();
+    return divText;
+}
+
+
+function getUserAgent() {
+    return navigator.userAgent;
+}
+
+function getBatteryData() {
+    getBatteryData().then(data => {
+    return data;
+  });
+}
